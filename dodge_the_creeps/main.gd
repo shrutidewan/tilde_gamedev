706815362510e9b6d4a9b25e1ad647f45a9c2c49
Shruti Dewan <90696645+shrutidewan@users.noreply.github.com>
2023-07-04 13:39:35 +0530

    add UI and complete main game scene

--- dodge_the_creeps/main.gd
+++ dodge_the_creeps/main.gd
@@ -7,11 +7,16 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 
 func _on_mob_timer_timeout():
@@ -42,11 +47,11 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
-	
-func _ready():
-	new_game()
+
+
